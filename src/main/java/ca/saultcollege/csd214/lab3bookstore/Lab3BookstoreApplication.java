1584ec4cca7b5b265635cb66a1e590427de49d4c
Thiago Silva <thiago.ervilha@gmail.com>
2025-10-31 10:39:50 -0400

    Initial setup for Lab 3 bookstore project

---
+++ src/main/java/ca/saultcollege/csd214/lab3bookstore/Lab3BookstoreApplication.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd214.lab3bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3BookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/saultcollege/csd214/lab3bookstore/Lab3BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd214.lab3bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
